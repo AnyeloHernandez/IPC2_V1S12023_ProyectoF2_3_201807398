c66d82b8b97c304369d5e08acd5777a55b4614c6
AnyeloHernandez <anyelo.g.hernandez@gmail.com>
2023-06-24 13:15:17 -0600

    Feat: Se añadio Navbar, Footer, login y logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ]
